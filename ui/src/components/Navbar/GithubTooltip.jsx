67959cf368ae75ad9d265ae40ff3cee8a95f6a20
Favella Pasquale <FavellaPa@valueteam.com>
2022-06-07 09:18:02 +0200

    update dithub repo url

--- ui/src/components/Navbar/GithubTooltip.jsx
+++ ui/src/components/Navbar/GithubTooltip.jsx
@@ -3,7 +3,7 @@ import { BsGithub } from 'react-icons/bs';
 const GitHubTooltip =  ()=>{
     return (
         <div className="tooltip tooltip-bottom before:text-xs before:content-[attr(data-tip)]" data-tip="github">
-            <a className="btn btn-ghost btn-circle normal-case btn-sm" href="https://github.com/nuvolaris/nuvolaris" target="_blank" rel="noopener noreferrer">
+            <a className="btn btn-ghost btn-circle normal-case btn-sm" href="https://github.com/Pasquale-Favella/Shortify" target="_blank" rel="noopener noreferrer">
             <BsGithub className='w-6 h-6'/>
             </a>
         </div>
